04ed7a320aa994bb90b8a5867e75f94742a3f914
Kushanrawindu <kushanrawindu@gmail.com>
2020-08-25 13:40:02 +0530

    adding form fields to the module

--- helper.php
+++ helper.php
@@ -22,6 +22,17 @@ class ModHelloWorldHelper
      */    
     public static function getHello($params)
     {
-        return 'Hello, World!';
+        return $db = JFactory::getDbo();
+        // Retrieve the shout
+        $query = $db->getQuery(true)
+                    ->select($db->quoteName('hello'))
+                    ->from($db->quoteName('#__helloworld'))
+                    ->where('lang = ' . $db->Quote('en-GB'));
+        // Prepare the query
+        $db->setQuery($query);
+        // Load the row.
+        $result = $db->loadResult();
+        // Return the Hello
+        return $result;
     }
 }
